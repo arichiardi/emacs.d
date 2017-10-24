25bd9fc4d4837bf7c1d4a7707a630a7988d71838
Andrea Richiardi <a.richiardi.work@gmail.com>
2017-10-24 15:18:32 -0700

    Update company-mode and add company-shell

--- packs/dev/foundation-pack/config/completion-conf.el
+++ packs/dev/foundation-pack/config/completion-conf.el
@@ -3,6 +3,14 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (live-add-pack-lib "company-mode")
+(live-add-pack-lib "company-shell")
+
 (require 'company)
+(require 'company-shell)
 
 (setq company-idle-delay 0)
+
+(add-hook 'shell-mode-hook
+          (lambda ()
+            (add-to-list (make-local-variable 'company-backends)
+                         '(company-shell company-shell-env))))
