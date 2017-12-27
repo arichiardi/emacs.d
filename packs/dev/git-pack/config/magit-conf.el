f6239e4c600133de91aa1b9923a37bec0a77375e
Andrea Richiardi <a.richiardi.work@gmail.com>
2017-12-27 12:45:51 -0800

    Upgrade Magit and add new dependencies

--- packs/dev/git-pack/config/magit-conf.el
+++ packs/dev/git-pack/config/magit-conf.el
@@ -1,20 +1,24 @@
 ;;; git-pack/magit-conf.el
 (live-add-pack-lib "with-editor")
+(live-add-pack-lib "magit-popup")
+(live-add-pack-lib "ghub")
 (live-add-pack-lib "magit/lisp")
 
-(require 'magit)
-(when (not (require 'magit-version nil t))
-  (message "`magit-version' not found, try to run make in magit/lisp"))
+(defvar live-magit-lisp-dir (expand-file-name (concat (live-pack-lib-dir) "magit/lisp")))
+(defvar live-magit-documentation-dir (expand-file-name (concat (live-pack-lib-dir) "magit/Documentation/")))
 
 (add-hook 'magit-log-edit-mode-hook
           (lambda ()
              (set-fill-column 72)
              (auto-fill-mode 1)))
 
-(defvar live-magit-documentation-dir (expand-file-name (concat (live-pack-lib-dir) "magit/Documentation")))
 
-(message (concat "Magit's Documentation: " live-magit-documentation-dir) )
+(load (concat live-magit-lisp-dir "/magit-autoloads"))
+
+(message (concat "Magit loaded: " (magit-version)))
 
 (with-eval-after-load 'info
   (info-initialize)
   (add-to-list 'Info-directory-list live-magit-documentation-dir))
+
+(setq magit-view-git-manual-method 'woman)
