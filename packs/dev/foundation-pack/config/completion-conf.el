46005d61f79a8ad286f728d14ecb5a212b111142
Andrea Richiardi <a.richiardi.work@gmail.com>
2018-01-05 13:06:02 -0800

    Add company-quickhelp

--- packs/dev/foundation-pack/config/completion-conf.el
+++ packs/dev/foundation-pack/config/completion-conf.el
@@ -4,9 +4,13 @@
 
 (live-add-pack-lib "company-mode")
 (live-add-pack-lib "company-shell")
+(live-add-pack-lib "pos-tip")
+(live-add-pack-lib "company-quickhelp")
+
 
 (require 'company)
 (require 'company-shell)
+(require 'company-quickhelp)
 
 (setq company-idle-delay 0)
 
@@ -14,3 +18,6 @@
           (lambda ()
             (add-to-list (make-local-variable 'company-backends)
                          '(company-shell company-shell-env))))
+
+(eval-after-load "company-mode"
+  (company-quickhelp-mode 1))
