8677ea51a75056043ea01d479a7f0d96772336c5
Andrea Richiardi <a.richiardi.work@gmail.com>
2018-02-21 12:35:44 -0800

    Use master zenburn for the company-quickhelp patch.

--- packs/dev/colour-pack/lib/zenburn.el
+++ packs/dev/colour-pack/lib/zenburn.el
@@ -8,9 +8,4 @@
   (require 'zenburn-theme)
   (load-theme 'zenburn t)
   ;; Fixes the selection background color
-  (set-face-attribute 'region nil :background "#666")
-  ;; Apparently the zenburn-emacs definitions are not enough
-  (eval-after-load 'company-quickhelp
-    '(progn
-       (setq company-quickhelp-color-background (cdr (assoc "zenburn-bg+1" zenburn-default-colors-alist)))
-       (setq company-quickhelp-color-foreground (cdr (assoc "zenburn-fg" zenburn-default-colors-alist))))))
+  (set-face-attribute 'region nil :background "#666"))
