e1aa8fbad09dc4739a3724e624d561e155e59731
Andrea Richiardi <a.richiardi.work@gmail.com>
2018-11-17 17:47:17 -0800

    Get rid of auto-complete leftovers

--- packs/dev/clojure-pack/info.el
+++ packs/dev/clojure-pack/info.el
@@ -1,3 +1,3 @@
 (live-pack-name "emacs-live-clojure-pack")
-(live-pack-version (live-version)) ;; keep in sync with Overtone Live
-(live-pack-description "A fully powered pack ready for Clojure hacking. Includes: paredit, cider, ac-nrepl, auto-complete, clojure-mode, mic-paren, rainbow-delimiters, popup-el, and more.")
+(live-pack-version (live-version))
+(live-pack-description "A fully powered pack ready for Clojure hacking.")
