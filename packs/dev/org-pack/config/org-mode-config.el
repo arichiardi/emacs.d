ecf6bfa01cf63f0614396f1463d88dd88ae746be
Andrea Richiardi <a.richiardi.work@gmail.com>
2018-11-17 17:57:15 -0800

    Get rid of yasnippet

--- packs/dev/org-pack/config/org-mode-config.el
+++ packs/dev/org-pack/config/org-mode-config.el
@@ -14,22 +14,6 @@
 (add-hook 'org-shiftdown-final-hook 'windmove-down)
 (add-hook 'org-shiftright-final-hook 'windmove-right)
 
-;; Yasnippet compatibility
-(add-hook 'org-mode-hook
-          (lambda ()
-            (org-set-local 'yas/trigger-key [tab])
-            (define-key yas/keymap [tab] 'yas/next-field-or-maybe-expand)))
-
-(defun yas/org-very-safe-expand ()
-  (let ((yas/fallback-behavior 'return-nil)) (yas/expand)))
-
-(add-hook 'org-mode-hook
-          (lambda ()
-            (make-variable-buffer-local 'yas/trigger-key)
-            (setq yas/trigger-key [tab])
-            (add-to-list 'org-tab-first-hook 'yas/org-very-safe-expand)
-            (define-key yas/keymap [tab] 'yas/next-field)))
-
 (eval-after-load "org"
   '(require 'ox-md nil t))
 
