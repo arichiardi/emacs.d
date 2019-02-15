5d3300753ca44688566f6ecb517fb39778677863
Andrea Richiardi <a.richiardi.work@gmail.com>
2019-02-15 14:14:25 -0800

    Move a bunch of the cider conf in here

--- packs/dev/clojure-pack/config/cider-conf.el
+++ packs/dev/clojure-pack/config/cider-conf.el
@@ -14,13 +14,26 @@
   (setq cider-popup-stacktraces t
         cider-popup-stacktraces-in-repl nil
         cider-repl-use-clojure-font-lock t
+        cider-prompt-for-project-on-connect nil
+        cider-repl-display-help-banner nil
+        cider-connection-message-fn 'cider-random-tip
         cider-overlays-use-font-lock t
         cider-repl-wrap-history t
         cider-repl-history-size 1000
-        cider-show-error-buffer t)
+        cider-use-tooltips nil
+        cider-show-error-buffer 'only-in-repl
+        cider-offer-to-open-cljs-app-in-browser nil
+        cider-invert-insert-eval-p t
+        cider-switch-to-repl-after-insert-p nil
+        cider-print-fn 'zprint)
 
   :config
-  (add-hook 'cider-mode-hook 'eldoc-mode)
-  (add-hook 'cider-mode-hook 'paredit-mode)
-  (add-hook 'cider-repl-mode-hook 'paredit-mode)
-  (add-hook 'cider-repl-mode-hook 'eldoc-mode))
+  (add-hook 'cider-mode-hook #'eldoc-mode)
+  (add-hook 'cider-mode-hook #'subword-mode)
+  (add-hook 'cider-mode-hook #'paredit-mode)
+  (add-hook 'cider-mode-hook #'company-mode)
+
+  (add-hook 'cider-repl-mode-hook #'paredit-mode)
+  (add-hook 'cider-repl-mode-hook #'eldoc-mode)
+  (add-hook 'cider-repl-mode-hook #'company-mode)
+  (add-hook 'cider-repl-mode-hook #'subword-mode))
