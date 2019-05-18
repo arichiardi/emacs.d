923fa22de3d0607ae610ac1b63d27850db6703b8
Andrea Richiardi <a.richiardi.work@gmail.com>
2019-05-17 17:41:10 -0700

    Add lsp-mode and lsp-java

---
+++ packs/dev/foundation-pack/config/lsp-conf.el
@@ -0,0 +1,13 @@
+(use-package lsp-mode
+  :commands lsp
+  :config
+  (setq lsp-enable-snippet nil))
+
+(use-package lsp-ui
+  :after lsp-mode
+  :commands lsp-ui-mode
+  :config
+  (add-hook 'lsp-mode-hook #'lsp-ui-mode))
+
+(use-package company-lsp
+  :requires company)
--- packs/dev/lang-pack/init.el
+++ packs/dev/lang-pack/init.el
@@ -1,6 +1,22 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Language Pack
 
+(use-package volatile-highlights
+  :config
+  (volatile-highlights-mode t))
+
+(use-package markdown-mode
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-open-command "function md() { pandoc \"$1\" | lynx -stdin; }; md"))
+
+(use-package lsp-java
+  :after lsp
+  :config
+  (add-hook 'java-mode-hook #'company-mode)
+  (add-hook 'java-mode-hook #'flycheck-mode))
+
 (live-load-config-file "flycheck-conf.el")
 (live-load-config-file "yaml-conf.el")
 (live-load-config-file "auto-modes.el")
