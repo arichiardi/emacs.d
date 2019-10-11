2cfd91806257fcf41d449d38c45539e04dc1a771
Andrea Richiardi <a.richiardi.work@gmail.com>
2019-10-11 15:28:56 -0700

    Assimilate browse-at-remote

--- packs/dev/git-pack/config/git-conf.el
+++ packs/dev/git-pack/config/git-conf.el
@@ -1,4 +1,7 @@
-(live-add-pack-lib "git-modes")
-(require 'gitattributes-mode)
-(require 'gitconfig-mode)
-(require 'gitignore-mode)
+(use-package git-modes
+  :defer t)
+
+(use-package browse-at-remote
+  :defer t
+  :bind (("C-c g b" . browse-at-remote)
+         ("C-c g k" . browse-at-remote-kill)))
