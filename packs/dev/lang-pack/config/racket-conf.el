70d026bd921e1563f66f7c360300e4db6e2979f5
Andrea Richiardi <a.richiardi.work@gmail.com>
2019-10-22 21:02:15 -0700

    Tweak racket-conf

--- packs/dev/lang-pack/config/racket-conf.el
+++ packs/dev/lang-pack/config/racket-conf.el
@@ -1,6 +1,18 @@
+(defun ar-emacs--define-racket-open-round-key ()
+  "Assign ( to racket-smart-open-bracket.
+
+  See https://github.com/greghendershott/racket-mode/issues/401."
+  (let ((oldmap (cdr (assoc 'paredit-mode minor-mode-map-alist)))
+        (newmap (make-sparse-keymap)))
+    (set-keymap-parent newmap oldmap)
+    (define-key newmap (kbd "(") #'racket-smart-open-bracket)
+    (make-local-variable 'minor-mode-overriding-map-alist)
+    (push `(paredit-mode . ,newmap) minor-mode-overriding-map-alist)))
+
 (defun ar-emacs--racket-conf-hook ()
   "Racket mode hook."
   (enable-paredit-mode)
+  (ar-emacs--define-racket-open-round-key)
   (company-mode-on)
   (eldoc-mode t)
   (subword-mode t)
@@ -10,9 +22,13 @@
 (use-package racket-mode
   :mode "\\.rkt\\'"
   :bind (("C-c M-j" . racket-run)
+         ("C-c SPC" . racket-align)
          :map racket-mode-map
          ("C-c C-c" . racket-send-last-sexp)
-         ("C-c C-z" . racket-run-and-switch-to-repl))
-  :init
-  (add-hook 'racket-mode-hook #'ar-emacs--racket-conf-hook)
-  (add-hook 'racket-repl-mode-hook #'ar-emacs--racket-conf-hook))
+         :map racket-repl-mode-map
+         ("C-w" . er/expand-region))
+  :hook
+  ((racket-mode . ar-emacs--racket-conf-hook)
+   (racket-repl-mode . ar-emacs--racket-conf-hook))
+  :custom
+  (racket-smart-open-bracket-enable t "Enable smart open bracket"))
