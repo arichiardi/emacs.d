5678c78a7eb47183661fd1c72ccc25267aaf70bc
Andrea Richiardi <a.richiardi.work@gmail.com>
2019-10-28 14:05:11 -0700

    Tweak lisp-conf

--- packs/dev/lang-pack/config/lisp-conf.el
+++ packs/dev/lang-pack/config/lisp-conf.el
@@ -1,9 +1,23 @@
+;;; lisp-conf.el --- Lisp Config
+
+;;; Commentary:
+
+;;; Code:
+
+(use-package common-lisp-mode
+  :mode "\\.lisp\\'")
+
 (use-package sly
-  :mode ("\\.lisp\\'" . sly-mode)
-  :bind ("C-c M-j" . sly)
+  :bind (("C-c M-j" . sly)
+         :map sly-mode-map
+         ("C-c M-j" . sly))
   :custom
+  (sly-kill-without-query-p t "Do not ask before killing")
   (sly-default-lisp 'sbcl "Set default implementation to Steel Bank Common Lisp")
   :config
   (push '(sbcl  ("sbcl") :coding-system utf-8-unix) sly-lisp-implementations))
 
-(use-package sly-quicklisp)
+(use-package sly-quicklisp
+  :defer t)
+
+;;; lisp-conf.el ends here
