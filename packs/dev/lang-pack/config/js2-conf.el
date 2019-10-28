bedceac0eab0dda2bebd3218783593ccf6c73035
Andrea Richiardi <a.richiardi.work@gmail.com>
2019-10-28 12:36:01 -0700

    Port to borg the whole js/json configuration

--- packs/dev/lang-pack/config/js2-conf.el
+++
@@ -1,5 +0,0 @@
-;;js2 mode
-(live-add-pack-lib "js2")
-(require 'js2-mode)
-
-(setq js2-basic-offset 2)
--- packs/dev/power-pack/config/json-conf.el
+++
@@ -1,8 +0,0 @@
-(live-add-pack-lib "json-snatcher")
-(live-add-pack-lib "json-reformat")
-(live-add-pack-lib "json-mode")
-
-(require 'json-mode)
-
-(setq json-reformat:indent-width 2)
-(setq js-indent-level 2)
