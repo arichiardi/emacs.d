7bf1a6f2cd3a610a6d550bf936f38d75efdcb3a5
Andrea Richiardi <a.richiardi.work@gmail.com>
2019-10-29 11:56:04 -0700

    Tweak lisp-conf another time

--- packs/dev/lang-pack/config/lisp-conf.el
+++ packs/dev/lang-pack/config/lisp-conf.el
@@ -4,18 +4,29 @@
 
 ;;; Code:
 
+(defun ar-emacs--configure-common-lisp ()
+  "Racket mode hook."
+  (enable-paredit-mode)
+  (company-mode-on)
+  (eldoc-mode t)
+  (subword-mode t)
+  (flyspell-prog-mode)
+  (rainbow-delimiters-mode-enable))
+
 (use-package common-lisp-mode
   :mode "\\.lisp\\'")
 
 (use-package sly
   :bind (("C-c M-j" . sly)
          :map sly-mode-map
-         ("C-c M-j" . sly))
+         ("C-c M-j" . sly)
+         ("C-c C-e" . sly-eval-last-expression))
+  :hook (sly-mode . ar-emacs--configure-common-lisp)
   :custom
   (sly-kill-without-query-p t "Do not ask before killing")
   (sly-default-lisp 'sbcl "Set default implementation to Steel Bank Common Lisp")
   :config
-  (push '(sbcl  ("sbcl") :coding-system utf-8-unix) sly-lisp-implementations))
+  (push '(sbcl  ("sbcl" "--noinform") :coding-system utf-8-unix) sly-lisp-implementations))
 
 (use-package sly-quicklisp
   :defer t
