093217ebdfd752405324f5d8ecd2cc79f43716b4
kapitan <a.richiardi.work@gmail.com>
2019-11-03 10:36:12 -0800

    Assimilate mic-paren and move to power-pack

--- packs/dev/power-pack/init.el
+++ packs/dev/power-pack/init.el
@@ -9,6 +9,7 @@
 (live-load-config-file "expand-region-conf.el")
 (live-load-config-file "ace-jump-mode-conf.el")
 (live-load-config-file "multiple-cursors-conf.el")
+(live-load-config-file "paren-conf.el")
 (live-load-config-file "git-gutter-conf.el")
 (live-load-config-file "wc-mode-conf.el")
 (live-load-config-file "zeal-at-point-conf.el")
