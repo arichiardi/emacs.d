b13ddff3127777653e4fad9c4e7f48199a04b7f6
kapitan <a.richiardi.work@gmail.com>
2019-11-06 16:15:45 -0800

    Assimilate jq-mode

--- packs/dev/power-pack/init.el
+++ packs/dev/power-pack/init.el
@@ -4,6 +4,9 @@
 (require 'iy-go-to-char)
 (require 'lively)
 
+(use-package jq-mode
+  :hook company-mode)
+
 (live-load-config-file "undo-tree-conf.el")
 (live-load-config-file "highlight-tail-conf.el")
 (live-load-config-file "expand-region-conf.el")
