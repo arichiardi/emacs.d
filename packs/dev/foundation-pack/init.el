a6213cabbc86d6269ed19bc0377617db0c240e57
Andrea Richiardi <a.richiardi.work@gmail.com>
2019-11-26 20:40:45 -0800

    Use a narrower regexp for .bashrc file

--- packs/dev/foundation-pack/init.el
+++ packs/dev/foundation-pack/init.el
@@ -2,7 +2,7 @@
   :config (dash-enable-font-lock))
 
 (use-package sh-mode
-  :mode ("\\.bats\\'" "\\.[a-zA-Z]+[rc|rc_]")
+  :mode ("\\.bats\\'" "\\.bashrc")
   :hook ((sh-mode . company-mode)
          (sh-mode . rainbow-delimiters-mode)))
 
