5af81a7ec91d47e026d4336b60def20b48a95228
Andrea Richiardi <a.richiardi.work@gmail.com>
2019-12-21 12:00:53 -0800

    Assimilate synosaurus

--- packs/dev/power-pack/init.el
+++ packs/dev/power-pack/init.el
@@ -14,3 +14,12 @@
 (live-load-config-file "wc-mode-conf.el")
 (live-load-config-file "eval-in-repl-conf.el")
 (live-load-config-file "zeal-at-point-conf.el")
+
+(use-package synosaurus
+  :bind (:map text-mode-map
+         ("C-c s l" . synosaurus-lookup)
+         ("C-c s i" . synosaurus-choose-and-insert)
+         ("C-c s r" . synosaurus-choose-and-replace))
+  :custom
+  (synosaurus-backend 'synosaurus-backend-wordnet "Set the wordnet backend.")
+  :hook (text-mode . synosaurus-mode))
