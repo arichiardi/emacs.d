9254ad491b8390344fa9a15a3585cae0499747a0
Andrea Richiardi <a.richiardi.work@gmail.com>
2020-01-16 16:56:35 -0800

    Add early-init.el tweaks from doom-emacs

---
+++ early-init.el
@@ -0,0 +1,40 @@
+;;; early-init.el --- The early initialization. -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;; This is take from doom-emacs.
+;;
+;; Emacs HEAD (27+) introduces early-init.el, which is run before init.el,
+;; before package and UI initialization happens.
+
+;;; Code:
+
+;; Defer garbage collection further back in the startup process
+(setq gc-cons-threshold most-positive-fixnum)
+
+;; In noninteractive sessions, prioritize non-byte-compiled source files to
+;; prevent the use of stale byte-code. Otherwise, it saves us a little IO time
+;; to skip the mtime checks on every *.elc file.
+(setq load-prefer-newer noninteractive)
+
+;; In Emacs 27+, package initialization occurs before `user-init-file' is
+;; loaded, but after `early-init-file'. Doom handles package initialization, so
+;; we must prevent Emacs from doing it early!
+;; (setq package-enable-at-startup nil)
+;; (advice-add #'package--ensure-init-file :override #'ignore)
+
+;; Prevent the glimpse of un-styled Emacs by disabling these UI elements early.
+(setq tool-bar-mode nil
+      menu-bar-mode nil)
+(when (fboundp 'set-scroll-bar-mode)
+  (set-scroll-bar-mode nil))
+
+;; Resizing the Emacs frame can be a terribly expensive part of changing the
+;; font. By inhibiting this, we easily halve startup times with fonts that are
+;; larger than the system default.
+(setq frame-inhibit-implied-resize t)
+
+;; Ignore X resources; its settings would be redundant with the other settings
+;; in this file and can conflict with later config (particularly where the
+;; cursor color is concerned).
+;; (advice-add #'x-apply-session-resources :override #'ignore)
