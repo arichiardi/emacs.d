22091ef3ca904c37ed265bff1f6aa11e43cac42f
Andrea Richiardi <a.richiardi.work@gmail.com>
2020-02-29 10:52:37 -0800

    Add plantuml support in org files

--- packs/dev/lang-pack/config/plantuml-conf.el
+++ packs/dev/lang-pack/config/plantuml-conf.el
@@ -1,4 +1,12 @@
 (use-package flycheck-plantuml
   :no-require t
   :after (:all flycheck plantuml-mode)
-  :config (flycheck-plantuml-setup))
+  :config
+  (flycheck-plantuml-setup))
+
+(use-package plantuml-mode
+  :commands (plantuml-mode org-src-lang-modes)
+  :config
+  ;; Enable plantuml-mode for PlantUML files
+  (add-to-list 'org-src-lang-modes '("plantuml" . plantuml))
+  (add-to-list 'auto-mode-alist '("\\.plantuml\\'" . plantuml-mode)))
