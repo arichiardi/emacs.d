88998051a9d5b562a64b35fcbef34fb6b1d0978c
Andrea Richiardi <a.richiardi.work@gmail.com>
2020-04-22 07:48:14 -0700

    Set png output as default for plantuml

--- packs/dev/lang-pack/config/plantuml-conf.el
+++ packs/dev/lang-pack/config/plantuml-conf.el
@@ -13,12 +13,16 @@
 (use-package plantuml-mode
   :mode ("\\.plantuml\\'")
   :commands (org-src-lang-modes)
+
   :config
   ;; Enable plantuml-mode for PlantUML files
   (add-to-list 'org-src-lang-modes '("plantuml" . plantuml))
   (use-package org
     :config
     (setq org-plantuml-jar-path "/usr/share/java/plantuml/plantuml.jar")
-    (org-babel-do-load-languages 'org-babel-load-languages '((plantuml . t)))))
+    (org-babel-do-load-languages 'org-babel-load-languages '((plantuml . t))))
+
+  :custom
+  ((plantuml-output-type "png" "Set the default output to png")))
 
 ;;; plantuml-conf.el ends here
