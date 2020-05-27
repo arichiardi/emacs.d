b2472a5054baa4c9d539f5dec9cfb42c46cb7f74
Andrea Richiardi <a.richiardi.work@gmail.com>
2020-05-27 15:46:50 -0700

    Hook up Lombok configuration in java-conf
    It is global for now.

--- packs/dev/lang-pack/config/java-conf.el
+++ packs/dev/lang-pack/config/java-conf.el
@@ -18,13 +18,23 @@
 
 (add-hook 'java-mode-hook #'ar-emacs--configure-java)
 
+(setq ar-emacs--project-lombok-jar
+      (expand-file-name "~/.m2/repository/org/projectlombok/lombok/1.18.12/lombok-1.18.12.jar"))
+
 (use-package lsp-java
   :after lsp-mode
   :config
   (put 'lsp-java-workspace-dir 'safe-local-variable #'stringp)
   (setq lsp-java-workspace-dir (expand-file-name "workspace" live-etc-dir))
   (setq lsp-java-workspace-cache-dir (expand-file-name ".cache/" lsp-java-workspace-dir))
-  (setq lsp-java-server-install-dir (expand-file-name "eclipse.jdt.ls/" lsp-server-install-dir)))
+  (setq lsp-java-server-install-dir (expand-file-name "eclipse.jdt.ls/" lsp-server-install-dir))
+
+  (setq lsp-java-vmargs
+        (append lsp-java-vmargs (list (concat "-javaagent:" ar-emacs--project-lombok-jar))))
+
+  ;; This is global, I don't like it
+  (setq lsp-java-import-gradle-jvm-arguments
+        `["-Xmx1G" ,(concat "-javaagent:" ar-emacs--project-lombok-jar)]))
 
 (use-package java-snippets)
 
