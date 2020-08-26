1d1f2fd74f223aa7da879dd2690abc44e45a1ace
Andrea Richiardi <a.richiardi.work@gmail.com>
2020-08-25 18:10:49 -0700

    Format python-conf a bit better

--- packs/dev/lang-pack/config/python-conf.el
+++ packs/dev/lang-pack/config/python-conf.el
@@ -7,12 +7,9 @@
 (use-package python
   :config
   (use-package eval-in-repl-python)
-
-  :bind (
-         :map python-mode-map
+  :bind (:map python-mode-map
          ("C-c C-c" . eir-eval-in-python)
          ("C-c C-k" . python-shell-send-buffer))
-
   :hook ((python-mode . company-mode)
          (python-mode . smartparens-strict-mode)))
 
