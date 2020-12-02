0cd4ba6b8f4a40a340aacf12890784a0eb19ff45
Andrea Richiardi <a.richiardi.work@gmail.com>
2020-12-02 12:23:34 -0800

    Use modus-vivendi-theme and clean up Emacs Live ones

--- packs/dev/colour-pack/init.el
+++ packs/dev/colour-pack/init.el
@@ -1,4 +1,8 @@
 (global-hl-line-mode 1)
 
-(live-load-config-file "colour-theme-conf.el")
 (live-load-config-file "live-fontify-hex-conf.el")
+
+(set-face-attribute 'default nil :height 120)
+
+(use-package modus-vivendi-theme
+  :config (load-theme 'modus-vivendi t))
