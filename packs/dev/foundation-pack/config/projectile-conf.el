b69d302b70da486f8389d348b75069aae696fc94
Andrea Richiardi <a.richiardi.work@gmail.com>
2020-12-15 15:59:27 -0800

    Assimilate projectile

---
+++ packs/dev/foundation-pack/config/projectile-conf.el
@@ -0,0 +1,42 @@
+;;; projectile-conf.el --- Projectile Config
+
+;;; Commentary:
+
+;;; Code:
+
+(live-add-pack-lib "projectile")
+(live-add-pack-lib "helm-projectile")
+(live-add-pack-lib "persp-projectile")
+
+(use-package projectile
+ :init
+ (setq projectile-cache-file (concat live-tmp-dir "projectile-cache"))
+ (setq projectile-known-projects-file (concat live-tmp-dir "projectile-known-projects.eld"))
+
+ :config
+  (setq projectile-mode-line '(:eval (format " Prj[%s]" (projectile-project-name))))
+  (projectile-register-project-type 'yarn '("package.json")
+                                    :compile "yarn install"
+                                    :test "yarn test"
+                                    :run "yarn start")
+  (setq projectile-globally-ignored-directories
+        (append '("node_modules") projectile-globally-ignored-directories))
+
+  :custom
+  (projectile-enable-caching t)
+  (projectile-switch-project-action 'projectile-find-file-dwim)
+  (projectile-idle-timer-seconds 60)
+  (projectile-create-missing-test-files t)
+  (projectile-completion-system 'helm)
+  (projectile-tags-backend 'ggtags))
+
+;; until we assimilate helm this is not going to work
+(use-package helm-projectile
+  :after (projectile helm))
+
+(use-package persp-projectile
+  :after (projectile perspective))
+
+(projectile-global-mode)
+
+;;; projectile-conf.el ends here
