2b2187ffe06839c6bd0138a7fc45a61daedb7199
Andrea Richiardi <a.richiardi.work@gmail.com>
2020-12-15 15:59:27 -0800

    Assimilate projectile

--- packs/dev/foundation-pack/init.el
+++ packs/dev/foundation-pack/init.el
@@ -33,6 +33,7 @@
 (live-load-config-file "zone-conf.el")
 (live-load-config-file "monkey-patch.el")
 (live-load-config-file "completion-conf.el")
+(live-load-config-file "projectile-conf.el")
 (live-load-config-file "ediff-conf.el")
 (live-load-config-file "lsp-conf.el")
 (live-load-config-file "paredit-conf.el")
