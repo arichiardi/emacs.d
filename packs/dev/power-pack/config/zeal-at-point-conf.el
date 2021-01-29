d212cb7515d0c8cb533a592566a302731e882bd5
Andrea Richiardi <a.richiardi.work@gmail.com>
2021-01-29 15:11:17 -0800

    Fix zeal-at-point sql hook problem

--- packs/dev/power-pack/config/zeal-at-point-conf.el
+++ packs/dev/power-pack/config/zeal-at-point-conf.el
@@ -1,18 +1,35 @@
-(defun ar-emacs--zeal-scheme-set-docset
-    ()
-  "Set the docset for the scheme family."
-  (let ((file-name (buffer-file-name)))
-    (pcase (file-name-extension file-name)
-      ((pred (string= "rkt"))
-       (setq zeal-at-point-docset "racket")))))
+;;; zeal-at-point-conf.el --- zeal-at-point Config
+
+;;; Commentary:
+
+;;; Code:
 
 (use-package zeal-at-point
+  :functions zeal-at-point
+  :defines (zeal-at-point-docset zeal-at-point-mode-alist)
   :bind (("C-c d z" . zeal-at-point))
-  :init
-  (add-hook 'geiser-mode-hook #'ar-emacs--zeal-scheme-set-docset)
 
-  :defines zeal-at-point-mode-alist
-  :custom
+  :config
   (push '(clojure-mode . "java") zeal-at-point-mode-alist)
   (push '(clojurescript-mode . ("cljs" "svg" "nodejs")) zeal-at-point-mode-alist)
-  (push '(terraform-mode . "terraform") zeal-at-point-mode-alist))
+  (push '(terraform-mode . "terraform") zeal-at-point-mode-alist)
+
+  (defun ar-emacs--zeal-set-docset-from-file-extension ()
+    "Set the Zeal docset from the file extension."
+    (let ((file-name (buffer-file-name)))
+      (pcase (file-name-extension file-name)
+        ((pred (string= "rkt"))
+         (setq zeal-at-point-docset "racket")))))
+
+  (defun ar-emacs--zeal-set-docset-from-sql-product ()
+    "Set the docset from the sql product."
+    (pcase sql-product
+      ((pred (string= "postgres"))
+       (setq-local zeal-at-point-docset "psql"))))
+
+  :hook
+  (geiser-mode . ar-emacs--zeal-set-docset-from-file-extension)
+  (sql-mode . ar-emacs--zeal-set-docset-from-sql-product)
+  (sql-interactive-mode . ar-emacs--zeal-set-docset-from-sql-product))
+
+;;; zeal-at-point-conf.el ends here
