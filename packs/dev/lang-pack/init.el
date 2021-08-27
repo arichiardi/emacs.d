82fa6e387a48469e55e6909c736b52240354da2d
Andrea Richiardi <a.richiardi.work@gmail.com>
2021-08-27 14:23:37 -0600

    Add typescript config but remove prettier stuff

--- packs/dev/lang-pack/init.el
+++ packs/dev/lang-pack/init.el
@@ -18,6 +18,28 @@
   ((json-reformat:indent-width 2 "Set width to 2")
    (js-indent-level 2 "Set indent level to 2")))
 
+(use-package web-mode
+  :mode ("\\.jsx\\'" "\\.tsx\\'")
+  :hook
+  ((web-mode . company-mode)
+   (web-mode . smartparens-strict-mode)
+   (web-mode . subword-mode)
+   (web-mode . eldoc-mode)
+   (web-mode . which-key-mode)
+   (web-mode . tide-setup)
+   (web-mode . tide-hl-identifier-mode)
+   (web-mode . ar-emacs--setup-tsx-company-backends))
+  :custom
+  (web-mode-content-types-alist '(("jsx" . "\\.[jt]sx?\\'")))
+  (web-mode-enable-auto-pairing t)
+  (web-mode-enable-auto-closing t)
+  (web-mode-enable-current-element-highlight t)
+  (company-transformers '(company-sort-by-backend-importance))
+  (flycheck-check-syntax-automatically '(save mode-enabled))
+  :config
+  (flycheck-add-mode 'typescript-tslint 'web-mode)
+  (flycheck-add-next-checker 'typescript-tide '(t . typescript-tslint) 'append))
+
 (live-load-config-file "flycheck-conf.el")
 (live-load-config-file "yaml-conf.el")
 (live-load-config-file "plantuml-conf.el")
@@ -29,5 +51,6 @@
 (live-load-config-file "java-conf.el")
 (live-load-config-file "javascript-conf.el")
 (live-load-config-file "go-conf.el")
+(live-load-config-file "typescript-conf.el")
 
 ;;; init.el ends here
