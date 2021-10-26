4b783f9a0e0215f6a3f07f88fc3e6fd78a2b2279
Andrea Richiardi <a.richiardi.work@gmail.com>
2021-10-26 10:21:52 -0600

    Hook up clj-refactor

--- packs/dev/clojure-pack/init.el
+++ packs/dev/clojure-pack/init.el
@@ -1,4 +1,9 @@
-;; Clojure Pack
+;;; init.el --- Clojure Pack
+
+;;; Commentary:
+
+;;; Code:
+
 (live-add-pack-lib "rainbow-delimiters")
 (require 'rainbow-delimiters)
 (require 'paredit)
@@ -11,3 +16,15 @@
 
 (live-load-config-file "clojure-conf.el")
 (live-load-config-file "cider-conf.el")
+
+(use-package clj-refactor
+  :config
+  (setq cljr-magic-require-namespaces (append '(("s" . "clojure.spec.alpha") ("ig" . "integrant.core") ("edn"   . "clojure.edn"))))
+  (setq cljr-project-clean-exceptions (append '("deps.edn" "build.clj")))
+  :custom
+  (cljr-auto-clean-ns nil "Better to leave this off for now")
+  (cljr-favor-prefix-notation t)
+  (cljr-clojure-test-declaration "[clojure.test :as test :refer [deftest testing is]]")
+  (cljr-magic-requires :prompt))
+
+;;; init.el ends here
