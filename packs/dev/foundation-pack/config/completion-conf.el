29b40924353cef1f65aec8ef998c49bd0d4970f6
Andrea Richiardi <a.richiardi.work@gmail.com>
2021-10-29 13:29:12 -0600

    Tweak company configuration

--- packs/dev/foundation-pack/config/completion-conf.el
+++ packs/dev/foundation-pack/config/completion-conf.el
@@ -8,7 +8,12 @@
   :diminish
   :defer t
   :custom
-  (company-idle-delay 0 "Set delay to zero")
+  (company-tooltip-limit 25)
+  (company-auto-commit t)
+  (company-require-match t)
+  (company-tooltip-flip-when-above t)
+  ;; (company-idle-delay .2)
+  (company-tooltip-idle-delay 0)
   :hook
   ((company-mode . (lambda () (company-quickhelp-mode 1)))
    (shell-mode . (lambda ()
