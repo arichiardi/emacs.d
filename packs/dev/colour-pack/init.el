bfa992741c3c7f63e65ba739a34e8c790a0eae6b
Andrea Richiardi <a.richiardi.work@gmail.com>
2021-10-29 09:35:28 -0600

    Add headers in colour-pack/init.el

--- packs/dev/colour-pack/init.el
+++ packs/dev/colour-pack/init.el
@@ -1,3 +1,9 @@
+;;; init.el --- Colour Pack
+
+;;; Commentary:
+
+;;; Code:
+
 (global-hl-line-mode 1)
 
 (live-load-config-file "live-fontify-hex-conf.el")
@@ -6,3 +12,5 @@
 
 (use-package modus-vivendi-theme
   :config (load-theme 'modus-vivendi t))
+
+;;; init.el ends here
