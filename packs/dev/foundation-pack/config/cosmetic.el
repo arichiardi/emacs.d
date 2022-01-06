2142fb3dd77cf7db1b087b93034a5e623cbde929
Andrea Richiardi <a.richiardi.work@gmail.com>
2022-01-05 18:34:21 -0700

    Revert cosmetic.el to something compatible with Emacs 28

--- packs/dev/foundation-pack/config/cosmetic.el
+++ packs/dev/foundation-pack/config/cosmetic.el
@@ -4,10 +4,12 @@
 
 ;;; Code:
 
-;; It seems emacsclient requires to set backend and font with elisp rather than .Xresources.
-;; However, the below settings need to match what is in .Xresources.
-(add-to-list 'default-frame-alist '(font-backend . ftcrhb))
-(set-face-attribute 'default nil :font "JetBrains Mono-8")
+;; FIXME: [emacs 29] It seems emacsclient requires to set backend and font with elisp alongside than
+;; .Xresources. However, we had to revert because of a segmentation fault.
+;; (add-to-list 'default-frame-alist '(font-backend . ftcrhb)
+;;
+;; NOTE: all the elisp settings need to match what is in .Xresources
+(set-face-attribute 'default nil :font "JetBrains Mono-11")
 
 (setq font-lock-maximum-decoration t)
 
