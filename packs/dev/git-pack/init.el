397255dd6944dc90a6bdff8faec7ee265e47dcb6
Andrea Richiardi <a.richiardi.work@gmail.com>
2022-08-30 15:43:47 -0600

    Assimilate git-timemachine

--- packs/dev/git-pack/init.el
+++ packs/dev/git-pack/init.el
@@ -1,4 +1,14 @@
-;;; git-pack/init.el
+;;; init.el --- Git Pack
+
+;;; Commentary:
+
+;;; Code:
 
 (live-load-config-file "git-conf.el")
 (live-load-config-file "magit-conf.el")
+
+(use-package git-timemachine
+  :custom
+  (git-timemachine-abbreviation-length 8))
+
+;;; init.el ends here
