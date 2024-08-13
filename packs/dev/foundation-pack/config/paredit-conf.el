7a45f58061ec6688302d82cb3ef0742dc6e846d4
Andrea Richiardi <a.richiardi.work@gmail.com>
2024-08-13 10:51:37 -0600

    Add more paredit configuration

--- packs/dev/foundation-pack/config/paredit-conf.el
+++ packs/dev/foundation-pack/config/paredit-conf.el
@@ -5,24 +5,6 @@
 
 ;;; Code:
 
-(with-eval-after-load "paredit"
-  (define-key paredit-mode-map (kbd "C-h") 'help-command)
-  (define-key paredit-mode-map (kbd "M-(") 'paredit-wrap-round)
-  ;; The following was overridding the below
-  ;; (define-key paredit-mode-map (kbd "M-)") 'paredit-forward-slurp-sexp)
-  (define-key paredit-mode-map (kbd "M-)") 'paredit-wrap-round-from-behind)
-  (define-key paredit-mode-map (kbd "C-<left>") 'paredit-forward-barf-sexp)
-  (define-key paredit-mode-map (kbd "C-<right>") 'live-paredit-forward-slurp-sexp-neatly)
-  (define-key paredit-mode-map (kbd "C-M-<left>") 'paredit-backward-slurp-sexp)
-  (define-key paredit-mode-map (kbd "C-M-<right>") 'paredit-backward-barf-sexp)
-  (define-key paredit-mode-map (kbd "M-<up>") 'live-paredit-previous-top-level-form)
-  (define-key paredit-mode-map (kbd "M-<down>") 'live-paredit-next-top-level-form)
-  (define-key paredit-mode-map (kbd "M-SPC ") 'live-paredit-tidy-trailing-parens)
-  (define-key paredit-mode-map (kbd "C-M-/") 'clju-toggle-ignore-form)
-  (define-key paredit-mode-map (kbd "C-M-f") 'paredit-forward)
-  (define-key paredit-mode-map (kbd "C-M-b") 'paredit-backward)
-  (define-key paredit-mode-map (kbd "M-<") 'ar-emacs-paredit-first-buffer-form))
-
 (require 'paredit)
 (require 'thingatpt)
 
@@ -162,6 +144,70 @@
 (defun live-paredit-copy-sexp-at-point ()
   (interactive)
     (kill-new (thing-at-point 'sexp)))
+
+(defun paredit--is-at-start-of-sexp ()
+  (and (looking-at "(\\|\\[")
+       (not (nth 3 (syntax-ppss))) ;; inside string
+       (not (nth 4 (syntax-ppss))))) ;; inside comment
+
+(defun paredit-duplicate-closest-sexp ()
+  (interactive)
+  ;; skips to start of current sexp
+  (while (not (paredit--is-at-start-of-sexp))
+    (paredit-backward))
+  (set-mark-command nil)
+  ;; while we find sexps we move forward on the line
+  (while (and (bounds-of-thing-at-point 'sexp)
+              (<= (point) (car (bounds-of-thing-at-point 'sexp)))
+              (not (= (point) (line-end-position))))
+    (forward-sexp)
+    (while (looking-at " ")
+      (forward-char)))
+  (kill-ring-save (mark) (point))
+  ;; go to the next line and copy the sexprs we encountered
+  (paredit-newline)
+  (yank)
+  (exchange-point-and-mark))
+
+(defun paredit-wrap-round-from-behind ()
+  (interactive)
+  (forward-sexp -1)
+  (paredit-wrap-round)
+  (insert " ")
+  (forward-char -1))
+
+;; http://whattheemacsd.com/setup-paredit.el-03.html
+(put 'paredit-forward-delete 'delete-selection 'supersede)
+(put 'paredit-backward-delete 'delete-selection 'supersede)
+(put 'paredit-open-round 'delete-selection t)
+(put 'paredit-open-square 'delete-selection t)
+(put 'paredit-doublequote 'delete-selection t)
+(put 'paredit-newline 'delete-selection t)
+
+(add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+(add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+(add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+
+(with-eval-after-load "paredit"
+  (define-key paredit-mode-map (kbd "C-h") 'help-command)
+  (define-key paredit-mode-map (kbd "M-(") 'paredit-wrap-round)
+  ;; The following was overridding the below
+  ;; (define-key paredit-mode-map (kbd "M-)") 'paredit-forward-slurp-sexp)
+  (define-key paredit-mode-map (kbd "M-)") 'paredit-wrap-round-from-behind)
+  (define-key paredit-mode-map (kbd "C-<left>") 'paredit-forward-barf-sexp)
+  (define-key paredit-mode-map (kbd "C-<right>") 'live-paredit-forward-slurp-sexp-neatly)
+  (define-key paredit-mode-map (kbd "C-M-<left>") 'paredit-backward-slurp-sexp)
+  (define-key paredit-mode-map (kbd "C-M-<right>") 'paredit-backward-barf-sexp)
+  (define-key paredit-mode-map (kbd "M-<up>") 'live-paredit-previous-top-level-form)
+  (define-key paredit-mode-map (kbd "M-<down>") 'live-paredit-next-top-level-form)
+  (define-key paredit-mode-map (kbd "M-SPC ") 'live-paredit-tidy-trailing-parens)
+  (define-key paredit-mode-map (kbd "C-M-/") 'clju-toggle-ignore-form)
+  (define-key paredit-mode-map (kbd "C-M-f") 'paredit-forward)
+  (define-key paredit-mode-map (kbd "C-M-b") 'paredit-backward)
+  (define-key paredit-mode-map (kbd "M-<") 'ar-emacs-paredit-first-buffer-form))
 
 ;; End:
 ;;; paredit-conf.el ends here
