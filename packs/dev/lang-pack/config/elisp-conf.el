cd37c9021b1a95fd8bee6e582e7c21bc66b4fae1
Andrea Richiardi <a.richiardi.work@gmail.com>
2024-08-13 11:09:04 -0600

    Enable company-mode in ielm-mode

--- packs/dev/lang-pack/config/elisp-conf.el
+++ packs/dev/lang-pack/config/elisp-conf.el
@@ -1,7 +1,10 @@
 (add-to-list 'auto-mode-alist '("\\.el$" . emacs-lisp-mode))
+
 (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
 (add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
+
 (add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'ielm-mode-hook #'company-mode-on)
 
 (define-key lisp-mode-shared-map (kbd "RET") 'reindent-then-newline-and-indent)
 
