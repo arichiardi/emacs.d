699b37805c3f44868d17e657669b4bd1120b7ada
Andrea Richiardi <a.richiardi.work@gmail.com>
2024-08-13 11:14:50 -0600

    Add some flyspell configuration

--- packs/dev/foundation-pack/config/spelling-conf.el
+++ packs/dev/foundation-pack/config/spelling-conf.el
@@ -1,7 +1,20 @@
+;;; spelling-conf.el --- Plantuml Config
+
+;;; Commentary:
+
+;;; Code:
+
 (use-package flyspell
   :config
   (progn
     ;; run flyspell with aspell, not ispell
     (setq ispell-program-name "aspell")
     (setq ispell-extra-args '("--sug-mode=ultra"))
-    (setq ispell-list-command "--list")))
+    (setq ispell-list-command "--list"))
+
+  :hook
+  (git-commit-mode . flyspell-mode)
+  (git-rebase-mode . flyspell-mode)
+  (markdown-mode . flyspell-mode))
+
+;;; spelling-conf.el ends here
