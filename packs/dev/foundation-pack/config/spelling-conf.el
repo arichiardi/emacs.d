b0b4a1ef03fe92b960021004ecd84378e73a9eee
Andrea Richiardi <a.richiardi.work@gmail.com>
2024-08-14 13:21:25 -0600

    Using hunspell for flyspell checks

--- packs/dev/foundation-pack/config/spelling-conf.el
+++ packs/dev/foundation-pack/config/spelling-conf.el
@@ -5,12 +5,11 @@
 ;;; Code:
 
 (use-package flyspell
-  :config
-  (progn
-    ;; run flyspell with aspell, not ispell
-    (setq ispell-program-name "aspell")
-    (setq ispell-extra-args '("--sug-mode=ultra"))
-    (setq ispell-list-command "--list"))
+  :custom
+  (ispell-local-dictionary "en_US")
+  (ispell-program-name "hunspell")
+  (ispell-extra-args '("--sug-mode=ultra"))
+  (ispell-list-command "--list")
 
   :hook
   (git-commit-mode . flyspell-mode)
