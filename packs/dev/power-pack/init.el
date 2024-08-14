f94f31cd26c728388bf6f080befa7dde632286ba
Andrea Richiardi <a.richiardi.work@gmail.com>
2024-08-14 13:21:10 -0600

    Assimilate org-present

--- packs/dev/power-pack/init.el
+++ packs/dev/power-pack/init.el
@@ -113,4 +113,17 @@
                     "aplay"
                     file))))
 
+(use-package org-present
+  :hook
+  (org-present-mode . (lambda ()
+                        (org-present-big)
+                        (org-display-inline-images)
+                        (org-present-hide-cursor)
+                        (org-present-read-only)))
+  (org-present-mode-quit . (lambda ()
+                             (org-present-small)
+                             (org-remove-inline-images)
+                             (org-present-show-cursor)
+                             (org-present-read-write))))
+
 ;;; init.el ends here
