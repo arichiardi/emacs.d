e600af31ff124404b292117e2e39b355b33d8301
Andrea Richiardi <a.richiardi.work@gmail.com>
2025-05-28 10:54:00 -0600

    Tweam mcp configuration

--- packs/dev/power-pack/config/llm-conf.el
+++ packs/dev/power-pack/config/llm-conf.el
@@ -189,21 +189,23 @@ Returns a list of cons cells (name . directive) for each .md file."
   (setf (alist-get 'org-mode gptel-response-prefix-alist) "@assistant\n"))
 
 (use-package mcp
-  :defines (mcp-hub-servers)
   :custom
   (mcp-log-level 'info)
+
+  :init
+  (setq mcp-hub-servers '())
 
   :config
   (setq mcp-hub-servers
         (append mcp-hub-servers
-         `(("time" . (:command "uvx" :args ("mcp-server-time"
-                                            "--local-timezone=Canada/Mountain")))
-           ("filesystem" . (:command "podman"
+         `(("mcp/filesystem" . (:command "podman"
                                      :args ("run" "-i" "--rm" "--network=host"
                                             "--mount" ,(concat "type=bind,src=" (exec-path-from-shell-getenv "HOME") "/git,dst=/projects")
                                             "mcp/filesystem"
                                             "/projects")))
-           ("context" . (:command "uvx"
-                                  :args ("--from" "llm-context" "lc-mcp")))))))
+           ("mcp/fetch" . (:command "podman"
+                                     :args ("run", "-i", "--rm", "mcp/fetch")))
+           ("mcp/time" . (:command "uvx" :args ("mcp-server-time"
+                                            "--local-timezone=Canada/Mountain")))))))
 
 ;;; llm-conf.el ends here
