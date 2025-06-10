1f4575e558685b71f2e85d9884a681327ddc0655
Andrea Richiardi <a.richiardi.work@gmail.com>
2025-06-10 09:59:18 -0600

    Assimilate and try lambda-themes

--- packs/dev/colour-pack/init.el
+++ packs/dev/colour-pack/init.el
@@ -8,28 +8,33 @@
 
 (live-load-config-file "live-fontify-hex-conf.el")
 
-;; Theme-agnostic setup can be configured thus, from:
-;; https://protesilaos.com/emacs/modus-themes#h:86f6906b-f090-46cc-9816-1fe8aeb38776
-
-;; (defvar ar-emacs-after-enable-theme-hook nil
-;;    "Normal hook run after enabling a theme.")
-
-;; (defun run-after-enable-theme-hook (&rest _args)
-;;    "Run `after-enable-theme-hook'."
-;;    (run-hooks 'ar-emacs-after-enable-theme-hook))
-
-;; (advice-add 'enable-theme :after #'run-after-enable-theme-hook)
-
 ;; From https://github.com/protesilaos/modus-themes/issues/112#issuecomment-2234622808
-(defun ar-emacs--maybe-run-modus-theme-hooks (theme)
-  (when (memq theme '(modus-vivendi modus-operandi))
-    (run-hooks 'modus-themes-after-load-theme-hook)))
-(add-hook 'enable-theme-functions #'ar-emacs--maybe-run-modus-theme-hooks)
+(defun ar-emacs--run-theme-hooks (theme)
+  "Run theme-specific hooks if THEME is one of the enabled themes."
+  (when (or (member 'modus-vivendi custom-enabled-themes)
+            (member 'modus-operandi custom-enabled-themes))
+    (run-hooks 'modus-themes-after-load-theme-hook))
+  (when (or (member 'lambda-dark custom-enabled-themes)
+            (member 'lambda-dask-faded custom-enabled-themes))
+    (run-hooks 'lambda-themes-after-load-theme-hook)))
 
-(use-package modus-themes
+(add-hook 'enable-theme-functions #'ar-emacs--run-theme-hooks)
+
+;; (use-package modus-themes
+;;   :demand
+;;   :config
+;;   (load-theme 'modus-vivendi :no-confirm)
+;;   (enable-theme 'modus-vivendi)
+;;   )
+
+(use-package lambda-themes
   :demand
+  :custom
+  (lambda-themes-set-italic-comments nil)
+  (lambda-themes-set-italic-keywords nil)
+  (lambda-themes-set-variable-pitch nil)
   :config
-  (load-theme 'modus-vivendi :no-confirm)
-  (enable-theme 'modus-vivendi))
+  (load-theme 'lambda-dark-faded :no-confirm)
+  (enable-theme 'lambda-dark))
 
 ;;; init.el ends here
